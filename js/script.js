a2ce8190ddf41342a4bf9f39b5d0f5981c364956
tetrycy <tetrycyyt@gmail.com>
2025-12-03 09:01:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -305,7 +305,7 @@ document.querySelectorAll('.desktop-icon').forEach(icon => {
                 window.open('folder-mati.html', '_blank', 'width=700,height=500,resizable=yes,scrollbars=yes');
                 break;
 case 'jakub-olkiewicz':
-    window.open('https://tetrycy.github.io/felietony-kuby/olkiewicz18.html', '_blank', 'width=700,height=500,resizable=yes,scrollbars=yes');
+    window.open('https://tetrycy.github.io/felietony-kuby/olkiewicz19.html', '_blank', 'width=700,height=500,resizable=yes,scrollbars=yes');
     break;
 case 'marian-fussball':
     window.open('https://www.tetrycy.com.pl/wlodarskifussball/index.html', '_blank', 'width=700,height=500,resizable=yes,scrollbars=yes');
@@ -314,7 +314,7 @@ case 'marian-fussball':
     window.open('betclic.html', '_blank', 'width=700,height=500,resizable=yes,scrollbars=yes');
     break;
 case 'fanatyk-pilkarstwa':
-    window.open('https://www.youtube.com/watch?v=6GUBoXCrDQM&t=1s', '_blank');
+    window.open('https://www.youtube.com/watch?v=_c1p8JcCCqc&t=11s', '_blank');
     break;
 
         }
